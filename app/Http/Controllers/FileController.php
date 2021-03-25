795f19350248094bc0499fcf2666dc76c017f5f4
junko5 <sasanoha.jun@gmail.com>
2021-03-26 04:20:21 +0900

    エクセルファイルの解析処理のためにテンポラリファイルに書き出す処理まで実装。

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -33,7 +33,7 @@ class FileController extends Controller
 
             $item = File::create([
                 'filename' => $filename,
-                'file' => $data
+                'file' => base64_encode($data)
             ]);
 
             return redirect(route('file.extract', $item->id));
@@ -56,7 +56,38 @@ class FileController extends Controller
             //反映済み
             return redirect(route('file.upload'))->with('msg_error', $id . ' 「' . $filename . '」はすでに反映されています。');
         }
-        //反映処理
+        //実績へ反映処理
+        
+        //エクセルファイルの内容
+        $data = $item->file;
+
+        //テンポラリファイル名
+        $fname_tmp = date('YmdHis'); //現在日時からファイル名作成
+        for ($i=0;$i<4;$i++) {
+            //4文字追加(65:A ～ 90:Z までの範囲)
+            $fname_tmp .= chr(mt_rand(65, 90));
+        }
+        $ext = substr($filename, strrpos($filename, '.')); //.付き拡張子
+
+        $path = "/tmp/".$fname_tmp;
+        //テンポラリファイル作成 base64のまま。ストリームのままだとdecodeエラー発生するため
+        file_put_contents($path, $data);
+
+        //ファイル読み出してdecode もう一回テンポラリ保存
+        $data_r = base64_decode(file_get_contents($path));
+        file_put_contents($path.$ext, $data_r);
+
+        // テストダウンロード処理
+        // return response($data_r, 200)
+        // ->header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        // ->header('Content-Disposition', 'inline; filename="'.$item->filename.'"');
+
+
+
+
+        //テンポラリファイル削除
+        unlink($path);
+        // unlink($path.$ext);
 
 
         return redirect(route('file.upload'))->with('msg_success', $id . ' 「' . $filename . '」を実績に反映しました。');
